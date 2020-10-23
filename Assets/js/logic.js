70296aaca8136526e159ad350b50edbba9a7f001
pmo48 <pmoriarty100@gmail.com>
2020-10-22 18:29:57 -0700

    added some questions

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,8 +1,20 @@
 // Button click to start game
 
 $(document).ready(function() {
+  
+  $("#startbutton").on("click", function () {
+    $("#question").text("TestQuestion1?");
+    $("#button1").text("TestOption1");
+    $("#button2").text("TestOption2");
+    $("#button3").text("TestOption3");
+    $("#button4").text("TestOption4");
+  })
 
-    $("#startbutton").on("click", function () {
-        console.log("test")
-    })
+  $("#button1").on("click", function () {
+    $("#question").text("TestQuestion2?");
+    $("#button1").text("TestQ2Option1");
+    $("#button2").text("TestQ2Option2");
+    $("#button3").text("TestQ2Option3");
+    $("#button4").text("TestQ2Option4");
+  })
 })
