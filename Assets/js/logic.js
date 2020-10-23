5de240f586643ed472582f500583669a543ef25f
pmo48 <pmoriarty100@gmail.com>
2020-10-22 22:53:02 -0700

    added button click logic

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,14 +1,15 @@
 // Button click to start game
 var $timer = 75;
+var $score = 0;
 
 $(document).ready(function() {
-  
-  $("#startbutton").on("click", function () {
 
+  $("#startbutton").on("click", function () {
+    $(".intro").empty();
     interval = setInterval(function() {
       $timer--;
       $(".timer").text("Time Left: " + $timer);
-    }, 1000);
+  }, 1000);
 
     $("#question").text("TestQuestion1?");
     $("#button1").text("TestOption1");
@@ -17,11 +18,29 @@ $(document).ready(function() {
     $("#button4").text("TestOption4");
   })
 
-  $("#button1").on("click", function () {
+
+  $("#button1, #button2, #button3, #button4").on("click", function () {
     $("#question").text("TestQuestion2?");
     $("#button1").text("TestQ2Option1");
     $("#button2").text("TestQ2Option2");
     $("#button3").text("TestQ2Option3");
     $("#button4").text("TestQ2Option4");
   })
+
+  $("#button1, #button2, #button3, #button4").on("click", function () {
+    $("#question").text("TestQuestion2?");
+    $("#button1").text("TestQ2Option1");
+    $("#button2").text("TestQ2Option2");
+    $("#button3").text("TestQ2Option3");
+    $("#button4").text("TestQ2Option4");
+  })
+
+  $("#button1, #button2, #button3, #button4").on("click", function () {
+    $("#question").text("TestQuestion45?");
+    $("#button1").text("TestQ2Option1");
+    $("#button2").text("TestQ2Option2");
+    $("#button3").text("TestQ2Option3");
+    $("#button4").text("TestQ2Option4");
+  })
+
 })
